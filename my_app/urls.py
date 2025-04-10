2edab1ce80d8297d07f5b1f40953d8034b62e92e
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-04-10 12:40:35 +0800

    Add files via upload

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import landingpage, chef_signup, chef_login, chef_homepage, user_signup, user_login, user_homepage, user_logout, add_recipe, get_recipes, edit_recipe, delete_recipe, delete_favorite, get_chefs, get_chef_recipes, rate_recipe, get_trending_recipes, get_top_rated_recipes, get_users
 
 urlpatterns = [
+    path('', landingpage, name='home'),
     path('landingpage/', landingpage, name='landingpage'),
     path('chef-signup/', chef_signup, name='chef_signup'),
     path("chef-login/", chef_login, name="chef_login"),
